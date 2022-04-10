0ae5faa4a108b77590aba153f9517efd3425e690
Allan Legemaate <alegemaate@gmail.com>
2022-04-10 14:20:20 -0400

    feat: integrate with gtag and adsense (#6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import React from "react";
 
 import "@/styles/global.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${
+        process.env.NEXT_PUBLIC_GTM_ID ?? ""
+      }`}
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID ?? ""}');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
