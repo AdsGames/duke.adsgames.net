7a7e4f4245930f6de1e4bbf276d1ac1fa8550dc9
Allan Legemaate <alegemaate@gmail.com>
2022-04-10 14:19:26 -0400

    feat: integrate with gtag and adsense

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -36,23 +36,5 @@ export const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({
         <a>A.D.S. Games</a>
       </Link>
     </div>
-
-    {/* <!-- Banner ad -->
-    <div class="fw-advertisement">
-      <script
-        async
-        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
-      <!-- Duke -->
-      <ins
-        class="adsbygoogle"
-        style="display: inline-block; width: 468px; height: 60px"
-        data-ad-client="ca-pub-2991077597167073"
-        data-ad-slot="5002627219"
-      ></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-    </div> */}
   </>
 );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,24 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="/favicon.png" rel="icon" type="image/png" />
-
-        {/* <script type="text/javascript">
-          var _gaq = _gaq || [];
-          _gaq.push(["_setAccount", "UA-34470404-1"]);
-          _gaq.push(["_trackPageview"]);
-
-          (function () {
-            var ga = document.createElement("script");
-            ga.type = "text/javascript";
-            ga.async = true;
-            ga.src =
-              ("https:" == document.location.protocol ? "https://" : "http://") +
-              "stats.g.doubleclick.net/dc.js";
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(ga, s);
-          })();
-        </script> */}
+        <script
+          async
+          crossOrigin="anonymous"
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${
+            process.env.NEXT_PUBLIC_ADSENSE_CLIENT ?? ""
+          }`}
+        />
       </Head>
+
       <body>
         <Main />
         <NextScript />
