51735fabcbf672288bdf459bc7d1adbd875e09f6
Allan Legemaate <alegemaate@gmail.com>
2022-04-10 16:12:53 -0400

    fix: gtm integration (#11)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,19 @@
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import Script from "next/script";
 import React from "react";
 
 import * as gtag from "@/lib/gtag";
 import "@/styles/global.css";
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  React.useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      strategy="afterInteractive"
+    />
+    <Script
+      dangerouslySetInnerHTML={{
+        __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -37,13 +21,12 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
               page_path: window.location.pathname,
             });
           `,
-        }}
-        id="gtag-init"
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} />
-    </>
-  );
-};
+      }}
+      id="gtag-init"
+      strategy="afterInteractive"
+    />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
